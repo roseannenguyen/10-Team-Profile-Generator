fac3a279bfee83c4f913e06478e7a4b10133a8d7
Roseanne Nguyen <rtnguye1@gmail.com>
2021-01-26 22:32:07 -0700

    working on looping questions prompt

--- app.js
+++ app.js
@@ -12,9 +12,18 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 const { restoreDefaultPrompts } = require("inquirer");
 
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
 let team = [];
 
-function renderHTML(){
+function renderHTMl(){
     fs.writeFileSync(outputPath, render(team))
 }
 
@@ -138,14 +147,15 @@ function renderIntern(answers) {
         });
 };
 
-function addProfile() {
+async function addProfile() {
     inquirer
         .prompt([
 
             {
                 type: "confirm",
                 name: "add",
-                message: "Would you like to add another team member",
+                message: "Would you like to add another team member?",
+                default: true,
             },
 
         ])
@@ -156,8 +166,6 @@ function addProfile() {
                 default:
                     return console.log("profile completed")
             }
-
-
         });
 };
 
@@ -168,14 +176,6 @@ function addProfile() {
 createProfile();
 
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
 
 
